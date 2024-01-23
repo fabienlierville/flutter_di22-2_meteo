1c9023ebeaceae28f0a35e6c4fc5b9cf74639771
fabien <flierville@cesi.fr>
2024-01-23 15:47:41 +0100

    Location

---
+++ lib/services/weather_service.dart
@@ -0,0 +1,7 @@
+class WeatherService{
+  // Créer les attributs de base API( baseUrl, apiKey)
+
+  // Créer la méthode qui retourne un Weather en interrogeant l'url :
+  // https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}
+
+}
