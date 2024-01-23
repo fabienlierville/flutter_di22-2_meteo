1738f2aaf76953ed45589871843c0990e2e7694f
fabien <flierville@cesi.fr>
2024-01-23 14:04:39 +0100

    Location

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,8 +22,57 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Météo"),
+        centerTitle: true,
       ),
-      body: Center(
+      drawer: Drawer(
+        child: Container(
+          color: Colors.blue,
+          child: Column(
+            children: [
+              DrawerHeader(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text("Villes", style: TextStyle(fontSize:  30, color: Colors.white),),
+                      ElevatedButton(
+                        onPressed: ajoutVille,
+                        child: Text(
+                          "Ajouter une ville",
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                        style: ButtonStyle(
+                            backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.white)),
+                      ),
+                    ],
+                  )
+              ),
+              ListTile(
+                onTap: null,
+                title: Text("Position Inconnue", style: TextStyle(color: Colors.white),textAlign: TextAlign.center,),
+              ),
+              Expanded(
+                  child: ListView.builder(
+                      itemCount: villes.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        String ville = villes[index];
+                        return ListTile(
+                          onTap: null,
+                          title: Text(ville, style: TextStyle(color: Colors.white),textAlign: TextAlign.center,),
+                          trailing: IconButton(
+                              icon: Icon(Icons.delete, color: Colors.white,),
+                              onPressed: (){
+                                deleteVille(ville);
+                              }
+                          ),
+
+                        );
+                      })),
+            ],
+          ),
+        ),
+    ),
+    body: Center(
         child: ElevatedButton(
           onPressed: (){
             print(villes);
@@ -59,8 +108,39 @@ class _HomePageState extends State<HomePage> {
   void deleteVille(String ville) async{
     SharedPreferences preferences = await SharedPreferences.getInstance();
     villes.remove(ville);
+    print(villes);
     await preferences.setStringList("villes", villes);
     getVilles();
+  }
+
+  Future<void> ajoutVille() {
+    String? villeSaisie;
+
+    return showDialog(
+        context: context,
+        builder: (BuildContext contextDialog) {
+          return SimpleDialog(
+            contentPadding: EdgeInsets.all(20),
+            title: Text("Ajoutez une ville"),
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                    labelText: "ville", hintText: "saisir ville"),
+                onChanged: (String value) {
+                  villeSaisie = value;
+                },
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if(villeSaisie != null){
+                      addVille(villeSaisie!);
+                      Navigator.pop(contextDialog);
+                    }
+                  },
+                  child: Text("Valider")),
+            ],
+          );
+        });
   }
 
 }
